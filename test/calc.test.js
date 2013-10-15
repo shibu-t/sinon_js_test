f10f2ac4297d1363acd42b64b6ccb1846e998fdc
shibu-t <cockpits526@gmail.com>
2013-10-16 04:14:20 +0900

    first commit

---
+++ test/calc.test.js
@@ -0,0 +1,9 @@
+﻿var expect = require('expect.js');
+var calc   = require('../lib/calc.js');
+
+describe('add', function(){
+    it('1と1を与えたら2を返す', function(){
+        var result = calc.add(1, 1);
+        expect(result).to.be(2);
+    });
+});
